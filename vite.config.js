32323e314671e3bc32802d19254d6bbd52506f7d
Ruslan Pepe <yypaau@gmail.com>
2025-05-01 19:21:07 +0300

    laravelWebSite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,16 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+      host: '0.0.0.0',
+      port: 5170,
+      origin: 'http://estatetraderu.ru:5170',
+      cors: true,
+      hmr: {
+        host: 'estatetraderu.ru',
+        protocol: 'ws',
+      },
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
@@ -18,3 +28,4 @@ export default defineConfig({
         }),
     ],
 });
+
